ba667ff6c0d0954ab957bf46804edeb8177f990c
magentareds <thething1845@yahoo.com>
2017-04-08 17:35:11 -0600

    Added sample rows to database for testing.

---
+++ seed.sql
@@ -0,0 +1,14 @@
+use bamazon;
+
+insert into product(product_name, department_name, price, stock_quantity)
+values 
+  ("Game Child", "Electronics", 34.99, 10),
+  ("Game Child Memory Card", "Electronics", 10.00, 5),
+  ("Kayak", "Outdoors and Sports", 340.95, 2),
+  ('29\" 35lbs. Draw Composite Bow', "Outdoors and Sports", 150.45, 4),
+  ("Curling Stone", "Outdoors and Sports", 19.99, 50),
+  ("Bamazon T-shirt", "Clothing", 9.95, 100),
+  ("Bamazon Men's Watch", "Clothing", 12.95, 20),
+  ("Bamazon Sweatpants", "Clothng", 8.95, 20),
+  ("Bamazon Socks 4-pair", "Clothing", 3.95, 15),
+  ("Bamazon Coffee Beans 50lbs.", "Food", 50.95, 200);

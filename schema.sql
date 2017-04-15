da8febe6e472de62e62a084f42e108b48bbe9ad9
magentareds <thething1845@yahoo.com>
2017-04-15 10:14:44 -0500

    Added initial versions

--- schema.sql
+++ schema.sql
@@ -2,11 +2,20 @@ create database bamazon;
 
 use bamazon;
 
+create table department(
+  department_id integer not null,
+  department_name varchar(50),
+  over_head_cost decimal(10,2),
+  total_sales decimal(10,2),
+  primary key(department_id)
+);
+
 create table product(
   item_id integer not null auto_increment,
   product_name varchar(50),
-  department_name varchar(50),
+  department_id integer not null,
   price decimal(10,2),
   stock_quantity integer,
-  primary key(item_id)
+  primary key(item_id),
+  foreign key (department_id) REFERENCES department(department_id)
 );
--- seed.sql
+++ seed.sql
@@ -1,14 +1,22 @@
 use bamazon;
 
-insert into product(product_name, department_name, price, stock_quantity)
+insert into department(department_id, department_name, over_head_cost, total_sales)
+values
+  (100, "Food", 3000.00, 0),
+  (200, "Electronics", 10000.00, 0),
+  (205, "Outdoors and Sports", 4000.00, 0),
+  (300, "Clothing", 10000.00, 0),
+  (500, "Misc", 2.00, 0);
+
+insert into product(product_name, department_id, price, stock_quantity)
 values 
-  ("Game Child", "Electronics", 34.99, 10),
-  ("Game Child Memory Card", "Electronics", 10.00, 5),
-  ("Kayak", "Outdoors and Sports", 340.95, 2),
-  ('29\" 35lbs. Draw Composite Bow', "Outdoors and Sports", 150.45, 4),
-  ("Curling Stone", "Outdoors and Sports", 19.99, 50),
-  ("Bamazon T-shirt", "Clothing", 9.95, 100),
-  ("Bamazon Men's Watch", "Clothing", 12.95, 20),
-  ("Bamazon Sweatpants", "Clothng", 8.95, 20),
-  ("Bamazon Socks 4-pair", "Clothing", 3.95, 15),
-  ("Bamazon Coffee Beans 50lbs.", "Food", 50.95, 200);
+  ("Game Child", 200, 34.99, 10),
+  ("Game Child Memory Card", 200, 10.00, 5),
+  ("Kayak", 205, 340.95, 2),
+  ('29\" 35lbs. Draw Composite Bow', 205, 150.45, 4),
+  ("Curling Stone", 205, 19.99, 50),
+  ("Bamazon T-shirt", 300, 9.95, 100),
+  ("Bamazon Men's Watch", 300, 12.95, 20),
+  ("Bamazon Sweatpants", 300, 8.95, 20),
+  ("Bamazon Socks 4-pair", 300, 3.95, 15),
+  ("Bamazon Coffee Beans 50lbs.", 100, 50.95, 200);
